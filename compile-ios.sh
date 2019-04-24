65e050356a717128c8c24077f851372a9e20ccfd
MidnightChip <midnightchips@gmail.com>
2019-04-24 15:17:31 -0600

    Works fully on ios 12

--- compile-ios.sh
+++ compile-ios.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+swiftc -target arm64-apple-ios11.4 -v -sdk $(xcrun --show-sdk-path --sdk iphoneos) *.swift -o trolldrop
+install_name_tool -add_rpath /usr/lib/libswift/4.1.2 trolldrop
+ldid2 -S./ent.xml trolldrop
+mkdir deb
+mkdir deb/usr
+mkdir deb/usr/bin
+mkdir deb/DEBIAN
+cp control deb/DEBIAN
+cp -R trolldrop deb/usr/bin/
+dpkg-deb -b deb trolldrop12.deb 
+rm -rf deb 
+rm trolldrop
 
-if [ -z "$CSID" ]; then
-    echo "put your code signing identity in the CSID environment variable"
-    exit 1
-fi
 
-swiftc -target arm64-apple-ios11.4 -v -sdk $(xcrun --show-sdk-path --sdk iphoneos) *.swift -o trolldrop-ios
-install_name_tool -add_rpath /usr/lib/libswift trolldrop-ios
-codesign -s "$CSID" -fv --entitlements ent.xml trolldrop-ios
+
